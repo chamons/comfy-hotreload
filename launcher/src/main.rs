77baf9e5da6b8e43a0ed62857af638db7e68aac4
Chris Hamons <chris.hamons@gmail.com>
2024-12-25 14:35:08 -0700

    dev: Split out render code

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 use macroquad::prelude::*;
 
+mod render;
+use render::*;
 mod texture_cache;
 
 #[cfg(feature = "hotreload")]
@@ -9,8 +11,7 @@ mod hotreload;
 #[cfg(feature = "hotreload")]
 use crate::hotreload::binding::{
     exports::example::host::game_api::{
-        ClickInfo, DrawLineCommand, ImageCommand, Key, KeyboardInfo, MouseInfo, Position,
-        RenderCommand, TextCommand,
+        ClickInfo, Key, KeyboardInfo, MouseInfo, Position, RenderCommand,
     },
     WebAssemblyContext, WebAssemblyInstance,
 };
@@ -18,8 +19,7 @@ use crate::hotreload::binding::{
 #[cfg(not(feature = "hotreload"))]
 pub use game::{
     exports::example::host::game_api::{
-        ClickInfo, DrawLineCommand, ImageCommand, Key, KeyboardInfo, MouseInfo, Position,
-        RenderCommand, TextCommand,
+        ClickInfo, Key, KeyboardInfo, MouseInfo, Position, RenderCommand,
     },
     Instance,
 };
@@ -254,53 +254,4 @@ fn get_mouse_state() -> MouseInfo {
             down: is_mouse_button_down(MouseButton::Middle),
         },
     }
-}
-
-fn handle_draw_line(line: DrawLineCommand) {
-    draw_line(
-        line.first.x,
-        line.first.y,
-        line.second.x,
-        line.second.y,
-        line.thickness,
-        Color {
-            r: line.color.r,
-            g: line.color.g,
-            b: line.color.b,
-            a: line.color.a,
-        },
-    )
-}
-
-async fn handle_image_command(image: ImageCommand, texture_cache: &mut TextureCache) {
-    // Ignore image loading errors and just skip render
-    if let Ok(texture) = texture_cache.get(&image.filename).await {
-        let mut params = DrawTextureParams::default();
-        if let Some(size) = image.size {
-            params.dest_size = Some(Vec2 {
-                x: size.width,
-                y: size.height,
-            })
-        }
-        draw_texture_ex(&texture, image.position.x, image.position.y, WHITE, params);
-    }
-}
-
-fn handle_text_command(text: TextCommand, font: &Font) {
-    draw_text_ex(
-        &text.text,
-        text.position.x,
-        text.position.y,
-        TextParams {
-            font: Some(&font),
-            font_size: text.size as u16,
-            color: Color {
-                r: text.color.r,
-                g: text.color.g,
-                b: text.color.b,
-                a: text.color.a,
-            },
-            ..Default::default()
-        },
-    );
 }
